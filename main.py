803516996bd93009fbd4b4dd71dca777f900889e
Milind-cod3-base <cntcod3@protonmail.com>
2022-02-26 23:26:53 +0530

    Important libraries imported

---
+++ main.py
@@ -0,0 +1,13 @@
+
+#Using Tkinter libraries of Python, this project creates a notepad. Tkinter will help with GUI.
+
+# Functions / Menus like 'file, edit, saving, opening, cut/paste etc' will be available in the GUI.
+
+import os
+import tkinter   #python's GUI package installed
+from tkinter import *
+
+from tkinter.messagebox import *    #its to write text / message in the notepad
+
+from tkinter.filedialog import *   #used for the dialog box to appear when you are opening file from anywhere in your system / saving your file in a particular position of place
+
