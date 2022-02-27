58ee796839666f784b08fbafb5df26a311dded31
Milind-cod3-base <cntcod3@protonmail.com>
2022-02-27 21:15:17 +0530

    Window height + Menu panel set

--- main.py
+++ main.py
@@ -17,4 +17,13 @@ class editorNotepad:
 
     __root = Tk()   #class's name mangling done. Can be called only by ._classname__namemangle
 
+    #setting window height and width of the editor
+    __ofHeight = 400
+    __ofWidth = 400
+    __ofTextArea = Text(__root)
+    __ofMenuBar = Menu(__root)
     
+    #creating three panels in the menu bar
+    __ofFileMenu = Menu(__ofMenuBar, tearoff=0)  #here tearoff is set to 0 to avoid the dotted lines at the top of the drop down menu.
+    __ofEditMenu = Menu(__ofMenuBar, tearoff=0)
+    __ofHelpMenu = Menu(__ofMenuBar, tearoff=0)
